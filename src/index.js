2eb4526bff30ada0f8183ba229f051c74290baf1
AngelOk1990 <pandafree@mail.ru>
2022-02-22 15:04:09 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = []
+  for (let i = 0; i < matrix.length; i++) {
+    for(j = 0; j < matrix[i].length; j++) {
+          let k = i;
+          result[k] = matrix[i];
+      k++;
+      }
+      return result;
+  }
 }
